1b44cd50a88d02f15e81f7324352ea082a9bb00f
Nikolaj <samsung831@gmail.com>
2024-11-23 16:57:49 +0100

    implemented hedaer design

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <div>This is the header</div>
-  )
+  return <header>
+    <div className='wrapper'>
+      <div className='cta'>
+        <p className='course-name'>Sound Desing Masterclass</p>
+        <h1>Lear the Art of Sound Design</h1>
+        <a href='#' className='demo-btn'>
+          Demo Lesson
+        </a>
+      </div>
+    </div>
+  </header>
 };
 
 export default Header
