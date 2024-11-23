a686a95274377a8d01d5b7142d4aa8931cbae01d
Nikolaj <samsung831@gmail.com>
2024-11-23 19:33:40 +0100

    added navbar logic and topics section

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./Header.css";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return <header>
     <div className='wrapper'>
+      <Navbar />
       <div className='cta'>
         <p className='course-name'>Sound Desing Masterclass</p>
         <h1>Lear the Art of Sound Design</h1>
